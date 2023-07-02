88fad9282e892508ec022e32bbda710daa69c699
bramey <bramey@esited.com>
2023-07-02 12:40:14 -0600

    Updates

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders app', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Float On/i);
-  expect(linkElement).toBeInTheDocument();
-});
