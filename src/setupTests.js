c82ebf4d825b9ddf5a514d20e377bbd90921c915
bramey <bramey@esited.com>
2023-07-11 12:07:29 -0600

    Updates

--- src/setupTests.js
+++
@@ -1,8 +0,0 @@
-import '@testing-library/jest-dom';
-
-const matchMedia = () => ({
-  matches: false,
-  addListener() {},
-  removeListener() {},
-})
-window.matchMedia = window.matchMedia || matchMedia
